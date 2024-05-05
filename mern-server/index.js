4821948d8c49c901659c915c64b38512d23af198
RishabSharma2003 <104881827+sharmarishab199@users.noreply.github.com>
2024-05-05 23:07:34 +0530

    initial commit

--- mern-server/index.js
+++ mern-server/index.js
@@ -95,9 +95,9 @@ app.get('/',(req,res)=>{
 const path = require('path');
 __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/mern-client/build')));
+    app.use(express.static(path.join(__dirname, '/mern-client/dist')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'mern-client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'mern-client', 'dist', 'index.html'));
     })
 }
 
